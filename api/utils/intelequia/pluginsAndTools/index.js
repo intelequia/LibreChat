f2199c0691f6a89b4e8e955ede13aa8d03d27340
Ubuntu <kenshinsamue@VM.xlb5amftuiue1lps30kwiz1p1h.ax.internal.cloudapp.net>
2024-10-17 14:59:10 +0000

    fix - renamed folder

--- api/utils/intelequia/pluginsAndTools/index.js
+++ api/utils/intelequia/pluginsAndTools/index.js
@@ -1,4 +1,4 @@
-const azureAiFunctionsPlugins = require('./azure-ai-functions');
+const azureAiFunctionsPlugins = require('./implementations');
 const pluginsAndTools = require('./pluginsAndTools');
 const specifications = require ('./specifications')
 module.exports = {
