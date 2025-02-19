132dd0b7035c03193b377f0aed41ad8ce4da7f9f
Enrique Pedroza <enrique.pedroza@intelequia.com>
2025-02-19 15:20:46 +0000

    hotfix

--- code-interpreter/CodeExecutor.cjs
+++ code-interpreter/CodeExecutor.cjs
@@ -65,7 +65,7 @@ Usage:
             ...params,
         };
         try {
-            const response = await fetch(EXEC_ENDPOINT, {
+            const response = await fetch("http://intelewriter-open-interpreter:8000/run", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -78,6 +78,7 @@ Usage:
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
             const result = await response.json();
+            console.log(result)
             let formattedOutput = '';
             if (result.output[0].content) {
                 formattedOutput += `stdout:\n${result.output[0].content}\n`;
