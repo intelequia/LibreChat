d31d77d74981984f580cc118b9ba5707d7427c7c
Pablo Suarez <47283527+PabloSR06@users.noreply.github.com>
2025-02-20 15:46:42 +0000

    fix: groups, roles and permissions

--- api/utils/intelequia/intelequiaConfigLoader.js
+++ api/utils/intelequia/intelequiaConfigLoader.js
@@ -15,20 +15,17 @@ const jwtDecode = require('jsonwebtoken/decode');
 async function intelequiaConfigLoader() {
   const url = process.env.REMOTE_CONFIG_FILE_URL;
 
-  if(url && url != ""){
+  if (url && url != "") {
     logger.info("[IntelequiaConfigLoader] Loading GraphClient and Permissions files ...")
-    const graphClient = await createGraphClient()
-    const result = await loadPermissionConfigFiles (url)
-    const {permissions, assistantCreator, functions} = result;
+    const result = await loadPermissionConfigFiles(url)
+    const { permissions, functions, assistantAdminRole } = result;
 
     global.myCache.set("functions", functions);
-    global.myCache.set("assistantCreator", assistantCreator);
     global.myCache.set("permissions", permissions);
+    global.myCache.set("assistantAdminRole", assistantAdminRole);
 
-    await loadUserGroupsFromGraph(graphClient)
-    await updateUsersRoles(graphClient)
   }
-  else{
+  else {
     logger.error(
       `[validateTools] fetch remote configuration file: Error fetching remote functions at URL "${url}": ${response.statusText} (HTTP ${response.status})`,
     );
@@ -41,11 +38,11 @@ async function intelequiaConfigLoader() {
  * @Author Enrique M. Pedroza Castillo 
  * @returns 
  */
-async function createGraphClient(){
+async function createGraphClient() {
   const tenantId = process.env.OPENID_TENANT_ID;
   const clientId = process.env.OPENID_CLIENT_ID;
   const clientSecret = process.env.OPENID_CLIENT_SECRET;
-  
+
   const credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
 
   const client = Client.initWithMiddleware({
@@ -59,80 +56,6 @@ async function createGraphClient(){
   return client;
 }
 
-/**
- * Checks GRAPH groups that has permissions to AssistantCreator and updates users roles to ADMIN
- * @Organization Intelequia
- * @Author Enrique M. Pedroza Castillo
- */
-async function updateUsersRoles(graphClient){
-
-  const adminGroups = global.myCache.get("assistantCreator"); 
-
-  try{
-    let usersToAdmin = [];
-    adminGroups.map(async (group) => {
-      let url = '/groups/' + group + "/members"
-
-      const {value} = await graphClient.api(url).get();
-
-      for (const user of value) {
-
-        const {mail} = user
-        const dbUserId = await User.findOne({ email:mail }).select('_id').lean();
-
-        if (dbUserId && !usersToAdmin.includes(dbUserId)) 
-          usersToAdmin.push(dbUserId);
-        
-      }
-      await User.updateMany({ _id: { $in: usersToAdmin } }, { $set: { role: 'ADMIN' } });
-      usersToAdmin = [];
-    })
-
-  }catch(e){
-    console.log(e)
-    logger.error(`[intelequiaConfigLoader] Fetching azure groups: Fetching remote configuration file at URL "${url}"`);
-  }
-}
-
-/**
- * Connects with Azure Graph Api to get all the groups of users within database by its email 
- * @Organization Intelequia
- * @Author Enrique M. Pedroza Castillo
- */
-async function loadUserGroupsFromGraph(graphClient){
-  logger.info(`[intelequiaConfigLoader] Fetching azure groups: Downloading groups from azure `);
-  
-  const users = await User.find({}).select('email').lean();
-  const userEmails = users.map(user => user.email);
-
-  const batchSize = 15;
-  for (let i = 0; i < userEmails.length; i += batchSize) {
-    // Get the batch of emails
-    const emailBatch = userEmails.slice(i, i + batchSize);
-    // Build the filter clause for this batch
-    const filter = emailBatch.map(email => `userPrincipalName eq '${email}'`).join(' or ');
-
-    try {
-      // Value is parameter inside Graph API response
-      const { value } = await graphClient.api(`/users?$select=id,mail&$filter=${filter}`).get();
-
-      for (const user of value) {
-        const result = await graphClient.api(`/users/${user.id}/joinedTeams`).get();
-        const userGroups = result.value;
-        const groupsIds = userGroups.map(group => group.id);
-
-        const dbUser = users.find(u => u.email === user.mail);
-        if (dbUser) {
-          global.myCache.set(dbUser._id.toString(), groupsIds, process.env.USER_GROUPS_CACHE_TTL);
-        }
-      }
-    } catch (error) {
-      console.error(error);
-      logger.error(`[intelequiaConfigLoader] Fetching azure groups: Error fetching user groups - ${error}`);
-    }
-  }
-}
-
 
 /**
  * Fetch file with Azure OpenAI functions specifications
@@ -143,14 +66,14 @@ async function loadUserGroupsFromGraph(graphClient){
  */
 
 async function loadPermissionConfigFiles(url) {
- 
+
   logger.info(`[validateTools] fetch remote configuration file: Fetching remote configuration file at URL "${url}"`);
-  try{
+  try {
     const response = await fetch(url);
-    if(response.ok){
+    if (response.ok) {
       return await response.json();
     }
-  } catch(e){
+  } catch (e) {
     console.log(e)
     logger.error(
       `[validateTools] fetch remote configuration file: Error fetching remote functions at URL "${url}": ${response.statusText} (HTTP ${response.status})`,
@@ -160,60 +83,37 @@ async function loadPermissionConfigFiles(url) {
 
 
 /**
- * Verifies if user groups are updated in cache, if not updates them with token specified groups
+ * Load groups from token to cache, and check the admin
  * @param {*} jwt 
+ * @param {*} user 
  * @Organization Intelequia
- * @Author Enrique M. Pedroza Castillo
+ * @Author Pablo Suárez Romero
  */
-async function updateUserInfoInCache(jwt,user) {
+async function updateUserInfoInCache(jwt, user) {
   let userIdToken = jwtDecode(jwt)
-  var userCachedGroups = global.myCache.get(user._id.toString());
 
-  if (userCachedGroups != null && userCachedGroups.length > 0 ){
-    const userGroupsInToken = userIdToken["groups"]
+  const adminGroup = global.myCache.get("assistantAdminRole");
 
-    // verify if two arrays are equal
-    const containsAll = (arr1, arr2) => 
-      arr2.every(arr2Item => arr1.includes(arr2Item))
-      
-    const sameMembers = (arr1, arr2) => 
-      containsAll(arr1, arr2) && containsAll(arr2, arr1);
+  const userGroupsInToken = userIdToken["groups"]
 
-    const areGroupsUpdated = sameMembers(userCachedGroups, userGroupsInToken)
-    
-    if(!areGroupsUpdated){
-      global.myCache.set(user._id.toString(), userIdToken.groups, process.env.USER_GROUPS_CACHE_TTL)
+  global.myCache.set(user._id.toString(), userGroupsInToken, process.env.USER_GROUPS_CACHE_TTL)
 
-      const adminGroups = global.myCache.get("assistantCreator");
-      const isAdmin = adminGroups.some(adminGroup => userIdToken.groups.includes(adminGroup));
-
-      if(isAdmin)
-        await User.updateOne({ _id: user._id }, { $set: { role: 'ADMIN' } });
-    };
-
-  }
-  else{
-    global.myCache.set(user._id.toString(), userIdToken.groups, process.env.USER_GROUPS_CACHE_TTL);  
-    const adminGroups = global.myCache.get("assistantCreator");
-    const isAdmin = adminGroups.some(adminGroup => userIdToken.groups.includes(adminGroup));
-
-    if(isAdmin)
-      await User.updateOne({ _id: user._id }, { $set: { role: 'ADMIN' } });
-  }
+  const role = userGroupsInToken.includes(adminGroup) ? 'ADMIN' : 'USER';
+  await User.updateOne({ email: user.email }, { $set: { role } });
 }
 
-async function saveGraphToken ( token, user ){
-  global.myCache.set(user.email.toString()+"-graph",token,process.env.USER_GROUPS_CACHE_TTL);
+async function saveGraphToken(token, user) {
+  global.myCache.set(user.email.toString() + "-graph", token, process.env.USER_GROUPS_CACHE_TTL);
 }
 
-async function SaveFunctionsInCache(AssistantList){
+async function SaveFunctionsInCache(AssistantList) {
   var functions = global.myCache.get("functions");
   AssistantList.forEach((assistant) => {
     assistant.tools.forEach((tool) => {
-      if(tool.function){
+      if (tool.function) {
         if (IsToolAFunction(tool.function.name)) {
           functions.forEach((func) => {
-            if(func.name == tool.function.name){
+            if (func.name == tool.function.name) {
               func.specifications = tool
             }
           });
@@ -224,22 +124,22 @@ async function SaveFunctionsInCache(AssistantList){
   global.myCache.set("functions", functions, process.env.FUNCTIONS_CACHE_TTL);
 }
 
-async function GetFunctionSpecification(ToolName){
-    const functions = global.myCache.get("functions");
-    const f = functions.find((func) => func.name === ToolName);
-    return f.specifications;  
+async function GetFunctionSpecification(ToolName) {
+  const functions = global.myCache.get("functions");
+  const f = functions.find((func) => func.name === ToolName);
+  return f.specifications;
 }
 
-async function IsToolAFunction(ToolName){
+async function IsToolAFunction(ToolName) {
   const functions = global.myCache.get("functions");
   return functions.some((func) => func.name === ToolName);
 }
 
-module.exports =  {
-  intelequiaConfigLoader, 
-  updateUserInfoInCache, 
-  IsToolAFunction, 
-  SaveFunctionsInCache, 
+module.exports = {
+  intelequiaConfigLoader,
+  updateUserInfoInCache,
+  IsToolAFunction,
+  SaveFunctionsInCache,
   GetFunctionSpecification,
   saveGraphToken
 };
