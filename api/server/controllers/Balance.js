ca848bb08bc015b210299c005cc6abfb0bd9b00c
Pablo Suarez <47283527+PabloSR06@users.noreply.github.com>
2025-03-12 14:33:57 +0000

    fix: balance api error

--- api/server/controllers/Balance.js
+++ api/server/controllers/Balance.js
@@ -1,14 +1,22 @@
 const Balance = require('~/models/Balance');
 
 async function balanceController(req, res) {
-  const { tokenCredits: balance = '' } =
-    (await Balance.findOne({ user: req.user.id }, 'tokenCredits').lean()) ?? {};
-  
-  const isBalanceEnabled = process.env.CHECK_BALANCE
 
-  balance = isBalanceEnabled == 'true' ? balance : 0;
-  console.log("balance")
-  res.status(200).send('' + balance);
+  const isBalanceEnabled = process.env.CHECK_BALANCE;
+
+  if (!isBalanceEnabled) {
+    return res.status(200).send('' + balance);
+  }      
+
+  
+  const balanceRecord = await Balance.findOne({ user: req.user.id }, 'tokenCredits').lean();
+  
+  const balance = balanceRecord?.tokenCredits ?? 0;
+  
+  console.info("Balance del usuario:", balance);
+  
+  return res.status(200).send('' + balance);
+
 }
 
 module.exports = balanceController;
