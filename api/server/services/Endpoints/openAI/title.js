37e0f9a102f2123096666c9858026bf07ba00ce6
Ubuntu <kenshinsamue@devMachine.2hknyeyf3j2etksg04naooyjrh.gvxx.internal.cloudapp.net>
2025-04-09 08:14:41 +0000

    Feature: added telemetry to conversation Title generation

--- api/server/services/Endpoints/openAI/title.js
+++ api/server/services/Endpoints/openAI/title.js
@@ -2,6 +2,7 @@ const { CacheKeys } = require('librechat-data-provider');
 const getLogStores = require('~/cache/getLogStores');
 const { isEnabled } = require('~/server/utils');
 const { saveConvo } = require('~/models');
+const { intelequiaCountTokens } = require('~/utils');
 
 const addTitle = async (req, { text, response, client }) => {
   const { TITLE_CONVO = 'true' } = process.env ?? {};
@@ -26,6 +27,35 @@ const addTitle = async (req, { text, response, client }) => {
     responseText: response?.text ?? '',
     conversationId: response.conversationId,
   });
+
+
+  /**
+   * Get telemetry data for the title generation
+   * @Organization Intelequia
+   * @Author Enrique M. Pedroza Castillo
+   */
+  const model = req.body.model
+  const responseText =  response?.text ?? ''
+  const { completion, prompt } = intelequiaCountTokens([text, responseText, title], model);
+  
+  /**
+   * Custom event to track when a user is not found
+   * @Organization Intelequia
+   * @Author Enrique M. Pedroza Castillo
+   */
+  global.appInsights.trackEvent({
+    name: 'Azure OpenAI Title Generation',
+    properties: {
+      userId: req.user.id,
+      userEmail: req.user.email,
+      charactersLength: title.length,
+      messageTokens: completion + prompt,
+      promptTokens: prompt,
+      completionTokens: completion,
+      model: model,
+    },
+  });
+
   await titleCache.set(key, title, 120000);
   await saveConvo(
     req,
